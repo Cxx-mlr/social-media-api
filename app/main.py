ab30d99d878b9c530c135de79c6ab22dbcfc081f
Cxx-mlr <papiernik019@gmail.com>
2022-06-14 01:16:57 -0500

    deploy Heroku

--- app/main.py
+++ app/main.py
@@ -20,4 +20,4 @@ async def add_process_time_header(request: Request, call_next):
 
 @app.get('/')
 def root():
-    return {'message':'Hello World'}
+    return {'message':'Successfully deployed from CI/CD pipeline'}
