871e1e431d47ff732b6d21b57b9bd3463a9003a3
Cxx-mlr <spapiernik12@gmail.com>
2024-10-06 20:21:10 -0500

    Add api module

---
+++ app/api/deps.py
@@ -0,0 +1,48 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+
+from sqlmodel import Session
+
+from app.core.db import engine
+from app.core.security import ALGORITHM
+from app.core.config import settings
+from app.models import TokenPayload
+from app.models import User
+
+import jwt
+
+from jwt import InvalidTokenError
+from pydantic import ValidationError
+
+from typing_extensions import Generator, Annotated
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl=f"{settings.API_V1_STR}/login/access-token"
+)
+
+def get_db() -> Generator[Session, None, None]:
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_db)]
+TokenDep = Annotated[str, Depends(oauth2_scheme)]
+
+def get_current_user(session: SessionDep, token: TokenDep):
+    try:
+        payload = jwt.decode(token, key=settings.SECRET_KEY, algorithms=[ALGORITHM])
+        token_payload = TokenPayload(**payload)
+    except (InvalidTokenError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials",
+        )
+    
+    user = session.get(User, token_payload.sub)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+    return user
+
+CurrentUserDep = Annotated[User, Depends(get_current_user)]
---
+++ app/api/main.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+from app.api.routers import login, users, posts, votes
+
+api_router = APIRouter()
+api_router.include_router(login.router, tags=["login"])
+api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(posts.router, prefix="/posts", tags=["posts"])
+api_router.include_router(votes.router, prefix="/votes", tags=["votes"])
