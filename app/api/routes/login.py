173d5074d526fc9d78acc9f3b762000be1e8b028
Cxx-mlr <spapiernik12@gmail.com>
2024-10-09 20:49:45 -0500

    app/api/routers/ --> app/api/routes/

---
+++ app/api/routes/login.py
@@ -0,0 +1,53 @@
+from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+
+from app.core.config import settings
+from app.core import security
+from app.models import Token, UserPublic
+
+from app.api.deps import SessionDep
+from app import crud
+
+from typing_extensions import Annotated
+from datetime import timedelta
+
+from app.api.deps import CurrentUserDep
+
+router = APIRouter()
+
+@router.post(path="/login/access-token")
+async def login_for_access_token(
+    session: SessionDep,
+    credentials: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    user = crud.authenticate(session=session, email=credentials.username, password=credentials.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect email or password",
+        )
+
+    return Token(
+        access_token=security.create_access_token(
+            user.id,
+            timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+        )
+    )
+
+from fastapi import Request
+
+@router.post("/login/test-token", response_model=UserPublic)
+async def test_access_token(current_user: CurrentUserDep, request: Request):
+    print(request.headers)
+    return current_user
+
+from fastapi import Request
+from app.api.deps import get_current_user, TokenDep
+
+@router.post("/login/test")
+async def test_headers(
+    session: SessionDep,
+    request: Request,
+    token: TokenDep
+):
+    user = get_current_user(session=session, token=token)
+    return request.headers
---
+++ app/api/routes/votes.py
@@ -0,0 +1,57 @@
+from fastapi import APIRouter, HTTPException, Depends, status
+from sqlmodel import select, Session
+
+from app.api.deps import SessionDep, CurrentUserDep
+from app.models import Post, Vote, VoteDirection, VotePublic
+
+import uuid
+
+router = APIRouter()
+
+@router.post(path="/{id}", response_model=VotePublic)
+def vote_post(
+    session: SessionDep,
+    current_user: CurrentUserDep,
+    id: uuid.UUID,
+    direction: VoteDirection
+):
+    post = session.exec(
+        select(Post)
+            .where(Post.id == id)
+    ).first()
+
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Post not found"
+        )
+
+    if post.owner_id == current_user.id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You cannot upvote your own post"
+        )
+    
+    for vote in current_user.votes:
+        if vote.post_id == post.id:
+            if vote.direction == direction:
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail=f"You have already {vote.direction}voted this post"
+                )
+            else:
+                vote.direction = direction
+            break
+    else:
+        vote = Vote(
+            user_id=current_user.id,
+            post_id=post.id,
+            direction=direction
+        )
+
+    session.add(vote)
+    session.commit()
+
+    session.refresh(vote)
+
+    return vote
