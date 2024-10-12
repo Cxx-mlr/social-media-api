8921bc6573ae61e049dfd6c0b9bee0fb87619eb2
Cxx-mlr <spapiernik12@gmail.com>
2024-10-11 19:30:26 -0500

    Add default admin user creation in init_db function

--- app/core/db.py
+++ app/core/db.py
@@ -1,9 +1,15 @@
-from sqlmodel import create_engine
+from sqlmodel import create_engine, SQLModel, Session, select
+from sqlalchemy.engine.base import Engine
 from app.core.config import settings
+
+from app.models import User, UserCreate
+from app import crud
 
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
-def init_db() -> None:
-    from sqlmodel import SQLModel
-
+def init_db(session: Session, engine: Engine = engine) -> None:
     SQLModel.metadata.create_all(engine)
+    user = session.exec(select(User).where(User.email == settings.FIRST_USER_EMAIL)).first()
+    if not user:
+        user_create = UserCreate(email=settings.FIRST_USER_EMAIL, password=settings.FIRST_USER_PASSWORD)
+        crud.create_user(session=session, user_create=user_create)
